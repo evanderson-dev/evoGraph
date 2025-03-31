627aad4220ac11820377eeb1338cb9702e1b2525
evanderson <axel_@DESKTOP-92MEPRT>
2025-03-31 11:32:16 -0300

    Update sidebar styles and improve submenu item labels for clarity

--- dashboard.php
+++ dashboard.php
@@ -57,9 +57,9 @@ $cargo = $_SESSION["cargo"];
             <div class="sidebar-item">
                 <a href="#" class="sidebar-toggle"><i class="fa-solid fa-plus"></i>Cadastro<i class="fa-solid fa-chevron-down submenu-toggle"></i></a>
                 <div class="submenu">
-                    <a href="#" onclick="openAddTurmaModal(); return false;"><i class="fa-solid fa-chalkboard"></i>Cadastrar Turma</a>
-                    <a href="cadastro_funcionario.php"><i class="fa-solid fa-user-plus"></i>Cadastrar Funcionário</a>
-                    <a href="#" onclick="openAddModal(); return false;"><i class="fa-solid fa-graduation-cap"></i>Cadastrar Aluno</a>
+                    <a href="#" onclick="openAddTurmaModal(); return false;"><i class="fa-solid fa-users"></i>Turma</a>
+                    <a href="cadastro_funcionario.php"><i class="fa-solid fa-user-plus"></i>Funcionário</a>
+                    <a href="#" onclick="openAddModal(); return false;"><i class="fa-solid fa-graduation-cap"></i>Aluno</a>
                 </div>
             </div>
             <?php endif; ?>
