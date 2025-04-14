2082edc85ed203057149fad10cadfbef309307dc
evanderson <axel_@DESKTOP-92MEPRT>
2025-04-14 08:40:22 -0300

    Update sidebar links to use new src directory paths for dashboard and my_profile pages

--- src/Views/dashboard.php
+++ src/Views/dashboard.php
@@ -62,9 +62,9 @@ $header_photo = file_exists($square_photo_path) ? $square_photo_path : $default_
 
         <!-- SIDEBAR -->
         <div class="sidebar" id="sidebar">
-            <a href="./Views/dashboard.php" class="sidebar-active"><i class="fa-solid fa-house"></i>Home</a>
+            <a href="./src/Views/dashboard.php" class="sidebar-active"><i class="fa-solid fa-house"></i>Home</a>
             <a href="#"><i class="fa-solid fa-chart-bar"></i>Relatórios</a>
-            <a href="./Views/my_profile.php"><i class="fa-solid fa-user-gear"></i>Meu Perfil</a>
+            <a href="./src/Views/my_profile.php"><i class="fa-solid fa-user-gear"></i>Meu Perfil</a>
 
             <?php if ($cargo === "Coordenador" || $cargo === "Diretor" || $cargo === "Administrador"): ?>
             <div class="sidebar-item">
@@ -86,7 +86,7 @@ $header_photo = file_exists($square_photo_path) ? $square_photo_path : $default_
 
         <main class="main-content" id="main-content">
             <div class="titulo-secao">
-                <span><a href="./Views/dashboard.php" class="home-link"><i class="fa-solid fa-house"></i></a>/ Gerenciamento de Turmas e Alunos</span>
+                <span><a href="./src/Views/dashboard.php" class="home-link"><i class="fa-solid fa-house"></i></a>/ Gerenciamento de Turmas e Alunos</span>
             </div>
 
             <?php if ($cargo === "Professor"): ?>
