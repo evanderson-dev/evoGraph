a716bcc8fa44848ddea47f6853c65fa718025b66
evanderson <axel_@DESKTOP-92MEPRT>
2025-04-15 22:41:32 -0300

    Fix asset paths in dashboard, funcionarios, and my_profile pages to remove redundant 'public' directory references

--- public/funcionarios.php
+++ public/funcionarios.php
@@ -29,14 +29,14 @@ $header_photo = file_exists($square_photo_path) ? $square_photo_path : $default_
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./public/assets/css/style.css" />
-    <link rel="stylesheet" href="./public/assets/css/dashboard.css" />
-    <link rel="stylesheet" href="./public/assets/css/sidebar.css" />
-    <link rel="stylesheet" href="./public/assets/css/modal-add-turma.css" />
-    <link rel="stylesheet" href="./public/assets/css/modal-add-funcionario.css" />
-    <link rel="stylesheet" href="./public/assets/css/modal-add-aluno.css" />
-    <link rel="stylesheet" href="./public/assets/css/modal-delete-funcionario.css" />
-    <link rel="stylesheet" href="./public/assets/css/modal-edit-funcionario.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/dashboard.css" />
+    <link rel="stylesheet" href="./assets/css/sidebar.css" />
+    <link rel="stylesheet" href="./assets/css/modal-add-turma.css" />
+    <link rel="stylesheet" href="./assets/css/modal-add-funcionario.css" />
+    <link rel="stylesheet" href="./assets/css/modal-add-aluno.css" />
+    <link rel="stylesheet" href="./assets/css/modal-delete-funcionario.css" />
+    <link rel="stylesheet" href="./assets/css/modal-edit-funcionario.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>evoGraph - Gerenciar Funcionários</title>
 </head>
@@ -159,14 +159,14 @@ $header_photo = file_exists($square_photo_path) ? $square_photo_path : $default_
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="public/assets/js/utils.js"></script>
-    <script src="public/assets/js/modal-add-funcionario.js"></script>
-    <script src="public/assets/js/modal-add-turma.js"></script>
-    <script src="public/assets/js/modal-add-aluno.js"></script>
+    <script src="./assets/js/utils.js"></script>
+    <script src="./assets/js/modal-add-funcionario.js"></script>
+    <script src="./assets/js/modal-add-turma.js"></script>
+    <script src="./assets/js/modal-add-aluno.js"></script>
     
-    <script src="public/assets/js/funcionarios.js"></script>
-    <script src="public/assets/js/dashboard.js"></script>
-    <script src="public/assets/js/ajax.js"></script>
+    <script src="./assets/js/funcionarios.js"></script>
+    <script src="./assets/js/dashboard.js"></script>
+    <script src="./assets/js/ajax.js"></script>
 
     <script>
         function toggleSidebar() {
