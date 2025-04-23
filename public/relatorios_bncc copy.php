cdb521e10811a3603b9fc2aab0fc7d7877e62d1c
evanderson <axel_@DESKTOP-92MEPRT>
2025-04-23 17:48:14 -0300

    Refactor CSS in relatorios_bncc to improve layout and styling; add basic reset and restructure header and footer styles

--- public/relatorios_bncc copy.php
+++ public/relatorios_bncc copy.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/dashboard.css" />
+    <!-- <link rel="stylesheet" href="./assets/css/dashboard.css" /> -->
     <link rel="stylesheet" href="./assets/css/sidebar.css" />
     <link rel="stylesheet" href="./assets/css/relatorios_bncc.css" />
-    <link rel="stylesheet" href="./assets/css/relatorio-google.css" />
     <link rel="stylesheet" href="./assets/css/modal-add-funcionario.css" />
     <link rel="stylesheet" href="./assets/css/modal-add-turma.css" />
     <link rel="stylesheet" href="./assets/css/modal-add-aluno.css" />
