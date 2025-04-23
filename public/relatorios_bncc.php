7f93c9d6d95ac15a6e9f44d75c5b370693f63022
evanderson <axel_@DESKTOP-92MEPRT>
2025-04-23 17:51:20 -0300

    Comment out dashboard CSS link in relatorios_bncc.php to prevent loading of unused styles

--- public/relatorios_bncc.php
+++ public/relatorios_bncc.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/dashboard.css" />
+    <!-- <link rel="stylesheet" href="./assets/css/dashboard.css" /> -->
     <link rel="stylesheet" href="./assets/css/sidebar.css" />
     <link rel="stylesheet" href="./assets/css/relatorios_bncc.css" />
     <link rel="stylesheet" href="./assets/css/modal-add-funcionario.css" />
