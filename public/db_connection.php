3775e5d92213485ec2e2f3ec44be67240126d885
evanderson <axel_@DESKTOP-92MEPRT>
2025-04-28 16:22:23 -0300

    Update database password in db_connection.php for improved security

--- public/db_connection.php
+++ public/db_connection.php
@@ -2,7 +2,7 @@
 // Configurações do banco de dados
 $host = "localhost";
 $username = "admEvoGraph";
-$password = "evoGraph@123";
+$password = "evoGraph123";
 $database = "evograph_db";
 
 // Criar a conexão
